c9a07ce85b31f28296ab51e4a6d3b7ee3782b2fe
Koray06 <koray.oke@hotmail.com>
2022-06-14 23:41:12 +0300

    recursiveFibonacci

---
+++ src/fibo/fibo.java
@@ -0,0 +1,21 @@
+package fibo;
+
+public class fibo {
+
+	
+	 static int fib(int n) {
+
+	        if (n == 1 || n == 2)
+	            return 1;
+
+	        return fib(n - 1) + (n - 2);
+	    }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(fib(9));
+	}
+
+}
